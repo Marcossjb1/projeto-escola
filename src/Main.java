2b93cf30bf35a9c1e14a2738b9cc636211a99a3d
unknown <ruchellguilherme100@gmail.com>
2023-05-09 23:00:14 -0300

    adicionando comentários com tarefas

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 package src;
 
 public class Main {
-    public static void main(String[] args) {
 
-    }
+  public static void main(String[] args) {
+    //TODO: CRIAR CLASSE FUNCIONÁRIO COM AS SEGUINTES VARIAVEIS(nome,idade,cpf,cargo,salario) -> Maycon
+    //TODO: CRIAR CLASSE ALUNO COM AS SEGUINTES VARIAVEIS(nome,idade,cpf,matricula,nota) -> Marcos
+    //TODO: CRIAR CLASSE PROFESSOR COM AS SEGUINTES VARIAVEIS(matéria) e estender a classe funcionario para a classe professor -> Maycon
+    //TODO: CRIAR CLASSE CronogramaAlimentarCantina(vocês escolhem as variaveis) e também criar uma função que ao chamar a mesma ela retorna a seguinte frase "HORA DO RANGO" -> Marcos
+    //TODO: CRIAR CLASSE MATÉRIA COM AS SEGUINTES VARIAVEIS(logica de programacao, java, banco de dados) -> Gui
+    //TODO: CRIAR MÉTODO PARA CALCULAR A MÉDIA DE UM ALUNO -> Gui
+  }
 }
