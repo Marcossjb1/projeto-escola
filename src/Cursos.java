7e1f61f7619807effd8b419501a6f72cacbc4f78
unknown <ruchellguilherme100@gmail.com>
2023-05-09 23:18:21 -0300

    criando enum com os cursos que um aluno pode receber

---
+++ src/Cursos.java
@@ -0,0 +1,9 @@
+package src;
+
+public enum Cursos {
+
+  LOGICA_DE_PROGRAMACAO,
+  JAVA,
+  BANCO_DE_DADOS;
+
+}
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ public class Main {
     //TODO: CRIAR CLASSE ALUNO COM AS SEGUINTES VARIAVEIS(nome,idade,cpf,matricula,nota) -> Marcos
     //TODO: CRIAR CLASSE PROFESSOR COM AS SEGUINTES VARIAVEIS(matéria) e estender a classe funcionario para a classe professor -> Maycon
     //TODO: CRIAR CLASSE CronogramaAlimentarCantina(vocês escolhem as variaveis) e também criar uma função que ao chamar a mesma ela retorna a seguinte frase "HORA DO RANGO" -> Marcos
-    //TODO: CRIAR CLASSE MATÉRIA COM AS SEGUINTES VARIAVEIS(logica de programacao, java, banco de dados) -> Gui
     //TODO: CRIAR MÉTODO PARA CALCULAR A MÉDIA DE UM ALUNO -> Gui
   }
 }
