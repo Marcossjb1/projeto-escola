ddb5d318bc5b319a1d750a008196a44bb85002e2
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-05-10 22:06:56 -0300

    Criado a classe Aluno suas Vars e a classe Cronograma da cantina com Vars e o retorno de função

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,2 +1,9 @@
-package src;public class Aluno {
+package src;
+
+public class Aluno {
+    public String nome;
+    public String idade;
+    public String cpf;
+    public int matricula;
+    public int nota;
 }
