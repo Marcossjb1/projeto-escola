512c719c977c73224852c17831af31141f536393
unknown <ruchellguilherme100@gmail.com>
2023-05-24 21:16:17 -0300

    removendo comentarios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import entity.Aluno;
 import entity.Professor;
 import java.util.Scanner;
 
@@ -5,41 +6,41 @@ public class Main {
 
   public static void main(String[] args) {
 
-      Scanner scanner = new Scanner(System.in);
-      Professor professor = new Professor();
-      //Aqui tu tem que criar um objeto aluno para conseguir chamar tua função
+    Scanner scanner = new Scanner(System.in);
+    Professor professor = new Professor();
+    Aluno aluno = new Aluno();
 
-      int opcao;
-      do{
-        System.out.println("Escolha uma opçao:  ");
-        System.out.println("1. Professor ");
-        System.out.println("2. Aluno");
-        System.out.println("3. Funcionario");
-        System.out.println("4. Cardapio da escola");
-        System.out.println("0. Sair");
-        opcao = scanner.nextInt();
-        switch (opcao){
-          case 1:
-            professor.menuProfessor(scanner);
-            break;
-          case 2:
-//Aí aqui tu chama teu objeto com a função, tipo "aluno.menuAluno"
-            break;
-          case 3:
-            System.out.println("Voce escolheu a opcao funcionario");
-            //Todo:Criar uma função para o funcionário
-            break;
-          case 4:
-            System.out.println("Voce escolheu a opcao cardapio");
-            //Todo:Criar uma função para o cardápio(peça para o usuário o cardápio do dia) -> Marcos
-            break;
-          case 0:
-            System.out.println("Encerrando programa");
-            break;
-          default:
-            System.out.println("opcao invalida, tente novamente");
-            break;
-        }
-      }while (opcao!= 0);
-    }
+    int opcao;
+    do {
+      System.out.println("Escolha uma opçao:  ");
+      System.out.println("1. Professor ");
+      System.out.println("2. Aluno");
+      System.out.println("3. Funcionario");
+      System.out.println("4. Cardapio da escola");
+      System.out.println("0. Sair");
+      opcao = scanner.nextInt();
+      switch (opcao) {
+        case 1:
+          professor.menuProfessor(scanner);
+          break;
+        case 2:
+          aluno.menuAluno(scanner);
+          break;
+        case 3:
+          System.out.println("Voce escolheu a opcao funcionario");
+          //Todo:Criar uma função para o funcionário
+          break;
+        case 4:
+          System.out.println("Voce escolheu a opcao cardapio");
+          //Todo:Criar uma função para o cardápio(peça para o usuário o cardápio do dia) -> Marcos
+          break;
+        case 0:
+          System.out.println("Encerrando programa");
+          break;
+        default:
+          System.out.println("opcao invalida, tente novamente");
+          break;
+      }
+    } while (opcao != 0);
+  }
 }
--- src/main/java/entity/Aluno.java
+++ src/main/java/entity/Aluno.java
@@ -4,39 +4,39 @@ import java.util.Scanner;
 
 public class Aluno {
 
-    public String nome;
-    public int idade;
-    public String cpf;
-    public int matricula;
-    public int nota1;
-    public int nota2;
+  public String nome;
+  public int idade;
+  public String cpf;
+  public int matricula;
+  public int nota1;
+  public int nota2;
 
-    public Aluno() {
-    }
-    public void menuAluno(){
-        Scanner scanner = new Scanner(System.in);
+  public Aluno() {
+  }
 
-        System.out.println("Digite o nome do aluno(a): ");
-        nome = scanner.next();
-        System.out.println("Digite a idade do aluno: ");
-        idade = scanner.nextInt();
-        System.out.println("Digite o Cpf do aluno: ");
-        cpf = scanner.nextLine();
-        System.out.println("Digite a matricula do aluno: ");
-        matricula = scanner.nextInt();
-        System.out.println("Digite a primeira nota do aluno(a): ");
-        nota1 = scanner.nextInt();
-        System.out.println("Digite a segunda nota do aluno(a): ");
-        nota2 = scanner.nextInt();
+  public void menuAluno(Scanner scanner) {
 
-        System.out.println("Olá, "+nome+" Seja bem-vindo.");
+    System.out.println("Digite o nome do aluno(a): ");
+    nome = scanner.next();
+    System.out.println("Digite a idade do aluno: ");
+    idade = scanner.nextInt();
+    System.out.println("Digite o Cpf do aluno: ");
+    cpf = scanner.nextLine();
+    System.out.println("Digite a matricula do aluno: ");
+    matricula = scanner.nextInt();
+    System.out.println("Digite a primeira nota do aluno(a): ");
+    nota1 = scanner.nextInt();
+    System.out.println("Digite a segunda nota do aluno(a): ");
+    nota2 = scanner.nextInt();
 
-        double media = calcularMedia(nota1, nota2);
-        //Acima eu estou atribuindo pra uma variavel chamada media o calculo da media, passando os valores que usuario digitou por parametro
-        //Agora é só tu criar um system out print, mostrando a media na tela
-    }
-    public double calcularMedia(double nota1, double nota2) {
-        return (nota1 + nota2) / 2;
-    }
+    System.out.println("Olá, " + nome + " Seja bem-vindo.");
+
+    double media = calcularMedia(nota1, nota2);
+    System.out.println("A sua media e " + media);
+  }
+
+  public double calcularMedia(double nota1, double nota2) {
+    return (nota1 + nota2) / 2;
+  }
 
 }
