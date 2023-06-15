366c0dd47806141d95c0494c71134b8e4457eeea
Marcossjb1 <marcosviniciussjb@outlook.com>
2023-06-14 22:59:31 -0300

    Melhorias na classe CardapioCantina

--- src/main/java/entity/CardapioCantina.java
+++ src/main/java/entity/CardapioCantina.java
@@ -2,11 +2,12 @@ package entity;
 
 public class CardapioCantina {
         public void menuCardapio() {
-        System.out.println("Energético......................R$13,50");
-        System.out.println("Refrigerante.....................R$4,50");
-        System.out.println("Salgadinho.......................R$7,90");
-        System.out.println("Bolacha..........................R$3,50");
-        System.out.println("Bala.............................R$1,00");
-        System.out.println("Chiclete.........................R$0,70");
+        System.out.println("1 - Energético......................R$13,50");
+        System.out.println("2 - Refrigerante.....................R$4,50");
+        System.out.println("3 - Salgadinho.......................R$7,90");
+        System.out.println("4 - Bolacha..........................R$3,50");
+        System.out.println("5 - Bala.............................R$1,00");
+        System.out.println("6 - Chiclete.........................R$0,70");
+        System.out.println("0 - Sair do cardápio");
     }
 }
